5f0646c0b9e5170b5961e5e4e1143dd8ceb7ecaf
Goffert van Gool <goffert@phusion.nl>
2016-07-17 18:29:16 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd /app
+
+# Install Bower packages
+if [ -f /app/bower.json ]; then
+	bower install
+fi
+
+case "$1" in
+shell)
+	echo "Dropping to shell"
+	exec bash
+	;;
+)
+*)
+	echo "polymer $@"
+	polymer $@
+	;;
+esac
