2fca1871c400787b3f76a924c58a12413f8ac371
Goffert van Gool <goffert@phusion.nl>
2016-07-17 20:20:39 +0200

    Serve on external container IP address

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@ shell)
 	echo "Dropping to shell"
 	exec bash
 	;;
+serve)
+	echo "Serving app"
+	shift
+	polymer serve -H $(ip addr | grep eth0 | awk '{print $2}' | egrep -o '([0-9]+\.){3}[0-9]+') $@
 *)
 	echo "polymer $@"
 	polymer $@
